091455f1d3c40d25b3af09519fc93680a41acbc6
jessamathews@gmail.com <jessamathews@gmail.com>
2022-12-21 20:48:32 -0500

    dec21

--- lecture-notes/Unit-3/diceGame.js
+++ lecture-notes/Unit-3/diceGame.js
@@ -17,7 +17,7 @@ const scoring = {
 
 //* State of Player
 // 3 states: "not rolled", "roll", "score"
-let state = {
+let dieState = {
     'not rolled': ['roll'],
     'roll': ['roll', 'score'],
     'score': ['not rolled'],
@@ -33,13 +33,52 @@ function scoreDie(dict, dieValue) {
 
 function rollDie() {
     // We'll generate a random number and increase our roll count.
-    dieRoll = Math.floor((Math.random() * 6) + 1);
-    // Random generates a value between 0-1. Multiplies that by 6 and then adds a whole value of 1 (+ 1) so that we start at 1 instead of zero.
-     //Math.floor(): Sets our value to a whole number (lowest).
-    numberOfRolls++;
 
+    dieRoll = Math.floor((Math.random() * 6) + 1);
+    //Math.random(): Random generates a value between 0-1. Multiplies that by 6 and then adds a whole value of 1 (+ 1) so that we start at 1 instead of zero.
+    //Math.floor(): Sets our value to a whole number (lowest).
+    numberOfRolls++;
 }
 
 function playState(task) {
     // pulling in our command and running our condtionals.
+
+    let valid = dieState[stateOfDie];
+    // default is "not rolled" but will be updated as players "roll die"
+
+    if(valid.includes(task)) {
+
+        stateOfDie = task;
+
+        if(stateOfDie === 'roll') {
+
+            if(numberOfRolls === 3) {
+                console.log(`You've rolled ${numberOfRolls} times and need to score. This roll doesn't count!`)
+            } else {
+                rollDie();
+                console.log(`You rolled a ${dieRoll}. Would you like to roll or finalize the score?`);
+
+                scoreDie(scoring, dieRoll);
+                console.log(`Number of rolls: ${numberOfRolls}`)
+            }
+
+        }
+
+        if(stateOfDie === 'score') {
+            dieRoll = 'notRolled';
+            numberOfRolls = 0;
+            console.log(`Final Score: ${score}`);
+            score = 0;
+        }
+
+    } else {
+        console.error(`Invalid state for the die: ${stateOfDie} to ${task}`);
+    }
 }
+
+playState('roll');
+playState('roll');
+playState('roll');
+playState('roll');
+playState('roll');
+playState('score');
