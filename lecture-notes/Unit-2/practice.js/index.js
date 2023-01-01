01e091d9bf8f8ce327097686949c7152df90f486
jessamathews@gmail.com <jessamathews@gmail.com>
2023-01-01 15:51:58 -0500

    labsNotes

---
+++ lecture-notes/Unit-2/practice.js/index.js
@@ -0,0 +1,27 @@
+/*
+Try the following in `index.js`:
+
+- Add two numbers together with the `+` operator
+- Multiply two numbers with the `*` operator
+- Divide two numbers using the `/` operator
+- Square a number by raising it to the power of `2**2`
+- Create some complex mathematical expressions using a combination of different operators and numerical values.
+
+Add you code below
+*/
+
+console.log(1 + 1);
+console.log(2 * 2);
+console.log(3 / 4);
+console.log(5 ** 2);
+console.log(typeof (5 ** 2));
+console.log(Math.random());
+console.log(Math.random() * 25);
+console.log(Math.floor(Math.random() * 25));
+console.log(Math.ceil(Math.random() * 25));
+console.log(Math.random() * 25);
+console.log((24 + 5) - (2 / 4));
+console.log(24 + (5 - 2) / 4);
+console.log(Math.ceil(24 + (5 - 2) / 4));
+
+
